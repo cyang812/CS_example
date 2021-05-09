3d2be4d4b8e617128760361f1012db47d1ba8b03
cyang <1018225817@qq.com>
2021-05-09 09:55:51 +0800

    Add LINQExample

---
+++ LINQExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace LINQExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var groupA = new[] { 3, 4, 5, 6, 7, 8 };
+            var groupB = new[] { 4, 5, 6, 7, 8, 9 };
+
+            //var someInts = from a in groupA
+            //               join b in groupB on a equals b
+            //               select a;
+
+            var someInts = from a in groupA
+                           join b in groupB on a equals b
+                           into groupAandB
+                           from c in groupAandB
+                           where c < 6
+                           select c;
+
+            foreach (var v in someInts)
+                Console.WriteLine($"{v}");
+        }
+    }
+}
